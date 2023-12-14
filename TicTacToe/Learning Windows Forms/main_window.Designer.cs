cf0c41a6ae57bc4814f7d09fb4b94c34e4fbd11a
markiloo <87264069+markiloo@users.noreply.github.com>
2023-12-14 11:43:35 +0800

    Add files via upload

---
+++ TicTacToe/Learning Windows Forms/main_window.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace main_window_methods
+{
+    partial class main_window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.box_2 = new System.Windows.Forms.Button();
+            this.box_3 = new System.Windows.Forms.Button();
+            this.box_6 = new System.Windows.Forms.Button();
+            this.box_5 = new System.Windows.Forms.Button();
+            this.box_4 = new System.Windows.Forms.Button();
+            this.box_9 = new System.Windows.Forms.Button();
+            this.box_8 = new System.Windows.Forms.Button();
+            this.box_7 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.box_1 = new System.Windows.Forms.Button();
+            this.Turn_label = new System.Windows.Forms.Label();
+            this.win_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // box_2
+            // 
+            this.box_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_2.Location = new System.Drawing.Point(570, 97);
+            this.box_2.Name = "box_2";
+            this.box_2.Size = new System.Drawing.Size(275, 250);
+            this.box_2.TabIndex = 1;
+            this.box_2.UseVisualStyleBackColor = true;
+            this.box_2.Click += new System.EventHandler(this.play_area_click);
+            // 
+            // box_3
+            // 
+            this.box_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_3.Location = new System.Drawing.Point(878, 97);
+            this.box_3.Name = "box_3";
+            this.box_3.Size = new System.Drawing.Size(275, 250);
+            this.box_3.TabIndex = 2;
+            this.box_3.UseVisualStyleBackColor = true;
+            this.box_3.Click += new System.EventHandler(this.play_area_click);
+            // 
+            // box_6
+            // 
+            this.box_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_6.Location = new System.Drawing.Point(878, 393);
+            this.box_6.Name = "box_6";
+            this.box_6.Size = new System.Drawing.Size(275, 250);
+            this.box_6.TabIndex = 5;
+            this.box_6.UseVisualStyleBackColor = true;
+            this.box_6.Click += new System.EventHandler(this.play_area_click);
+            // 
+            // box_5
+            // 
+            this.box_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_5.Location = new System.Drawing.Point(570, 393);
+            this.box_5.Name = "box_5";
+            this.box_5.Size = new System.Drawing.Size(275, 250);
+            this.box_5.TabIndex = 4;
+            this.box_5.UseVisualStyleBackColor = true;
+            this.box_5.Click += new System.EventHandler(this.play_area_click);
+            // 
+            // box_4
+            // 
+            this.box_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_4.Location = new System.Drawing.Point(251, 393);
+            this.box_4.Name = "box_4";
+            this.box_4.Size = new System.Drawing.Size(275, 250);
+            this.box_4.TabIndex = 3;
+            this.box_4.UseVisualStyleBackColor = true;
+            this.box_4.Click += new System.EventHandler(this.play_area_click);
+            // 
+            // box_9
+            // 
+            this.box_9.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_9.Location = new System.Drawing.Point(878, 693);
+            this.box_9.Name = "box_9";
+            this.box_9.Size = new System.Drawing.Size(275, 250);
+            this.box_9.TabIndex = 8;
+            this.box_9.UseVisualStyleBackColor = true;
+            this.box_9.Click += new System.EventHandler(this.play_area_click);
+            // 
+            // box_8
+            // 
+            this.box_8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_8.Location = new System.Drawing.Point(570, 693);
+            this.box_8.Name = "box_8";
+            this.box_8.Size = new System.Drawing.Size(275, 250);
+            this.box_8.TabIndex = 7;
+            this.box_8.UseVisualStyleBackColor = true;
+            this.box_8.Click += new System.EventHandler(this.play_area_click);
+            // 
+            // box_7
+            // 
+            this.box_7.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_7.Location = new System.Drawing.Point(251, 693);
+            this.box_7.Name = "box_7";
+            this.box_7.Size = new System.Drawing.Size(275, 250);
+            this.box_7.TabIndex = 6;
+            this.box_7.UseVisualStyleBackColor = true;
+            this.box_7.Click += new System.EventHandler(this.play_area_click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(570, 1015);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(275, 77);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // box_1
+            // 
+            this.box_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box_1.Location = new System.Drawing.Point(251, 97);
+            this.box_1.Name = "box_1";
+            this.box_1.Size = new System.Drawing.Size(275, 250);
+            this.box_1.TabIndex = 10;
+            this.box_1.UseVisualStyleBackColor = true;
+            this.box_1.Click += new System.EventHandler(this.play_area_click);
+            // 
+            // Turn_label
+            // 
+            this.Turn_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Turn_label.Location = new System.Drawing.Point(564, 39);
+            this.Turn_label.Name = "Turn_label";
+            this.Turn_label.Size = new System.Drawing.Size(281, 40);
+            this.Turn_label.TabIndex = 11;
+            this.Turn_label.Text = "Current Turn: ";
+            this.Turn_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // win_label
+            // 
+            this.win_label.AutoSize = true;
+            this.win_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.win_label.Location = new System.Drawing.Point(1265, 997);
+            this.win_label.Name = "win_label";
+            this.win_label.Size = new System.Drawing.Size(56, 48);
+            this.win_label.TabIndex = 12;
+            this.win_label.Text = "...";
+            // 
+            // main_window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1454, 1140);
+            this.Controls.Add(this.win_label);
+            this.Controls.Add(this.Turn_label);
+            this.Controls.Add(this.box_1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.box_9);
+            this.Controls.Add(this.box_8);
+            this.Controls.Add(this.box_7);
+            this.Controls.Add(this.box_6);
+            this.Controls.Add(this.box_5);
+            this.Controls.Add(this.box_4);
+            this.Controls.Add(this.box_3);
+            this.Controls.Add(this.box_2);
+            this.Name = "main_window";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button box_2;
+        private System.Windows.Forms.Button box_3;
+        private System.Windows.Forms.Button box_6;
+        private System.Windows.Forms.Button box_5;
+        private System.Windows.Forms.Button box_4;
+        private System.Windows.Forms.Button box_9;
+        private System.Windows.Forms.Button box_8;
+        private System.Windows.Forms.Button box_7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button box_1;
+        private System.Windows.Forms.Label Turn_label;
+        private System.Windows.Forms.Label win_label;
+    }
+}
+
---
+++ TicTacToe/Learning Windows Forms/main_window.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace main_window_methods
+{
+    public partial class main_window : Form
+    {
+        Random random = new Random();
+        bool isPlayerTurn = true;
+        string[] markers = { "X", "O" };
+
+        public main_window()
+        {
+            InitializeComponent();
+            isPlayerTurn = random.Next(0, 2) == 0;
+            Turn_label.Text = "Current Turn: " + markers[isPlayerTurn ? 0:1];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Button> play_area = new List<Button> { box_1, box_2, box_3, box_4, box_5, box_6, box_7, box_8, box_9 };
+
+            foreach (var box in play_area)
+            {
+                box.Enabled = true;
+                box.Text = "?";
+                box.BackColor = Color.LightGray;
+            }
+            win_label.Text = "...";
+        }
+        private void play_area_click(object sender, EventArgs e)
+        {
+            Button box_clicked = (Button)sender;
+
+            if (isPlayerTurn)
+            {
+                box_clicked.Text = markers[isPlayerTurn ? 0 : 1];
+                box_clicked.BackColor = Color.Aqua;
+                
+            }
+            else if (isPlayerTurn == false)
+            {
+                box_clicked.Text = markers[isPlayerTurn ? 0 : 1];
+                box_clicked.BackColor = Color.Red;   
+            }
+
+            Turn_label.Text = "Current Turn: " + markers[isPlayerTurn ? 1 : 0];
+
+            check_win(markers[isPlayerTurn ? 0 : 1]);
+
+            isPlayerTurn = !isPlayerTurn;
+            box_clicked.Enabled = false;
+
+        }
+
+        private void check_win(string marker)
+        {
+            List<Button> play_area = new List<Button> { box_1, box_2, box_3, box_4, box_5, box_6, box_7, box_8, box_9 };
+
+            bool HorizonalWin = (box_1.Text == marker && box_2.Text == marker && box_3.Text == marker) || (box_4.Text == marker && box_5.Text == marker && box_6.Text == marker) || (box_7.Text == marker && box_8.Text == marker && box_9.Text == marker);
+            bool VerticalWin = (box_1.Text == marker && box_4.Text == marker && box_7.Text == marker) || (box_2.Text == marker && box_5.Text == marker && box_8.Text == marker) || (box_3.Text == marker && box_6.Text == marker && box_9.Text == marker);
+            bool DiagonalWin = (box_5.Text == marker && box_1.Text == marker && box_9.Text == marker) || (box_5.Text == marker && box_3.Text == marker && box_7.Text == marker);
+
+            if (HorizonalWin || VerticalWin || DiagonalWin)
+            {
+                foreach(Button button in play_area) 
+                {
+                    button.Enabled = false;
+                    win_label.Text = marker + " Wins!";
+                }
+            }
+        }
+
+
+    }
+}
